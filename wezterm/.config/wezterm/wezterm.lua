8a61f3009a9194858db0ded82d3206c69f8302fd
Sagar Ali <sagar794@gmail.com>
2026-01-26 17:57:23 -0600

    feat: add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,38 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.audible_bell = "Disabled"
+config.color_scheme = "iceberg-dark"
+config.font = wezterm.font("Iosevka Term")
+config.font_size = 17
+config.hide_tab_bar_if_only_one_tab = true
+config.scrollback_lines = 10000
+config.window_decorations = "RESIZE"
+config.window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
+
+-- Leader key (Ctrl+F, then press second key within 1 second)
+config.leader = { key = "f", mods = "CTRL", timeout_milliseconds = 1000 }
+
+local act = wezterm.action
+config.keys = {
+	-- Pane splitting
+	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+	-- Pane navigation (vim-style)
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+
+	-- Pane management
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+
+	-- Tab management
+	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+}
+
+return config
