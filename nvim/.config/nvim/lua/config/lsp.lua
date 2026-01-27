754a5e3ebbd0e8f6f0c1755e0b297e0052e4ec7f
Sagar Ali <sagar794@gmail.com>
2026-01-26 18:23:50 -0600

    fix: prefix should show up once

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -13,20 +13,21 @@ vim.lsp.enable({
 vim.diagnostic.config({
   signs = false,
   virtual_text = {
-    prefix = '⚫',
+    prefix = '',
     format = function(diagnostic)
       local max_width = 100
       local message = diagnostic.message:gsub("\n", " ")
       if #message > max_width then
-        return string.sub(message, 1, max_width) .. "..."
+        return "● " .. string.sub(message, 1, max_width) .. "..."
       end
-      return message
+      return "● " .. message
     end,
   },
   underline = true,
   float = {
     border = "rounded",
     source = true,
+    prefix = '',
   },
 })
 
@@ -38,7 +39,7 @@ vim.api.nvim_create_autocmd("CursorHold", {
     local diagnostics = vim.diagnostic.get(0, {lnum = vim.api.nvim_win_get_cursor(0)[1] - 1 })
     for _, diagnostic in ipairs(diagnostics) do
       if #diagnostic.message > 100 then
-        vim.diagnostic.open_float(nil, { focus = false })
+        vim.diagnostic.open_float({ focus = false, prefix = "" })
         return
       end
     end
